4c3a7c52051ccb6362f08ece51faf132e2b56941
Aleksandr <sas099239@gmail.com>
2019-05-22 13:22:16 +0300

    Create app component

---
+++ client/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import App from '../imports/components/app';
+
+import './main.html';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
