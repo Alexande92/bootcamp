3f417913a1b01c76c39ded020c84c83010f317a5
Aleksandr <sas099239@gmail.com>
2019-06-03 17:15:21 +0300

    create base routing

--- client/main.jsx
+++ client/main.jsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Meteor } from 'meteor/meteor';
-
-import AppContainer from '../imports/ui';
-
-import store from './store';
-
-Meteor.startup(() => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <AppContainer />
-    </Provider>,
-    document.getElementById('root')
-  );
-});
+import '../imports/routes';
---
+++ imports/routes.js
@@ -0,0 +1,23 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { mount } from 'react-mounter';
+
+import AppContainer from './ui/containers/app-container';
+
+FlowRouter.route('/page/:id', {
+  name: 'Shows.show',
+  action (params) {
+    console.log('Flow');
+    mount(AppContainer, {
+      page: params.id,
+    });
+  },
+});
+
+FlowRouter.route('/', {
+  name: 'Shows.show',
+  action () {
+    mount(AppContainer, {
+      page: 1,
+    });
+  },
+});
