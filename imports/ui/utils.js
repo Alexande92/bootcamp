0a45a09e21b15ee23118505729a10d16ab26764f
Aleksandr <sas099239@gmail.com>
2019-06-05 17:13:21 +0300

    improve pagination logic

--- imports/ui/utils.js
+++ imports/ui/utils.js
@@ -3,69 +3,40 @@ export const getPaginatedArray = (current, total) => {
     return [];
   }
 
-  const pages = [];
+  let pages = [];
   const step = 3;
 
-  if (total < step * step) {
+  if (total < step * 2 + 1) {
     for (let i = 1; i <= total; i += 1) {
       pages.push(i);
     }
     return pages;
   }
 
-  // pages.push(1, 2, 3, current, total - 2, total - 1, total);
+  pages.push(1, 2, 3, total - 2, total - 1, total);
 
-  // if (current + step < total) {
-  //   pages.push(current + 1);
-  // }
-  //
-  // if (current - step > 0) {
-  //   pages.push(current - 1);
-  // }
-  //
-  // pages = deleteDuplicateValues(pages);
-  //
-  // pages.sort((a, b) => a - b);
-  //
-  //
-  // for (let i = 0; i < pages.length - 1; i += 1) {
-  //   if (pages[i + 1] - pages[i] > 1) {
-  //     newArr[i] = '...';
-  //     newArr[i + 1] = pages[i];
-  //   } else {
-  //     newArr[i] = pages[i]
-  //   }
-  //
-  //
-  // }
-  //
-  //
-  //
-  // return pages;
-
-
-  if (current - step < 0 || current + step >= total) {
-    pages.push(1, 2, 3, '...', total - 3, total - 2, total - 1, total);
+  if (pages.indexOf(current) === -1) {
+    pages.push(current);
   }
 
-  if (current - step === 2) {
-    pages.push(1, 2, 3, current - 1, current, current + 1, '...', total - 2, total - 1, total);
+  if (current + step < total && pages.indexOf(current + 1) === -1) {
+    pages.push(current + 1);
   }
 
-  if (current - step === 1) {
-    pages.push(1, 2, 3, current, current + 1, '...', total - 2, total - 1, total);
+  if (current - step > 0 && pages.indexOf(current - 1) === -1) {
+    pages.push(current - 1);
   }
 
-  if (current - step === 0) {
-    pages.push(1, 2, 3, current + 1, '...', total - 2, total - 1, total);
-  }
+  pages.sort((a, b) => a - b);
 
-  // if (current + step === total) {
-  //   pages.push(1, 2, 3, '...', current, total - 2, total - 1, total);
-  // }
-
-  if (!pages.length) {
-    pages.push(1, 2, 3, '...', current - 1, current, current + 1, '...', total - 2, total - 1, total);
+  for (let i = 1; i < pages.length; i += 1) {
+    if (pages[i] - pages[i - 1] > 1) {
+      pages = [
+        ...pages.slice(0, i),
+        '...',
+        ...pages.slice(i),
+      ];
+    }
   }
 
   return pages;
